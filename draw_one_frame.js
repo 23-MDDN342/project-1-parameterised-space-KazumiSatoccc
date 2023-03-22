97f90a3f624e56c7c82c492ba4a9b440a95167ef
Kazumi <satokazumi69@gmail.com>
2023-03-22 18:01:41 +1300

    gradient update

--- draw_one_frame.js
+++ draw_one_frame.js
@@ -15,55 +15,47 @@ sizee = 15;
 adder = 4;
 
 function draw_one_frame() {
-// 	//let polyRotate = map(frameCount, 0, radians(deg24), 0, radians(deg60));
-//   background(255);
-//   //hexagons
-//   translate(200, 200);
-  
-//   for (let i = 0; i < 10; i++){
-// 	push();
-// 	//angleMode(DEGREES);
-// 	strokeWeight(4);
-// 	//stroke(frameCount + 10*i);
-// 	rotate(frameCount * i);
-// 	strokeJoin(ROUND);
-// 	noFill();
-//     polygon(0, 0, polySize + 10 * i, 6)
-	
-// 	pop();
+	colorMode(RGB,255,100,100,100);
+	background(200);
+	translate(200,200);
 
-//   }
+	radialGradient(
+		0, 0,0,
+		0, 0,350,
+		color(250,230,80,100), //startcolor
+		color(250,180,0,100) //endcolor
+	  );
+	noStroke();
+	ellipse(0,0,200,200); //draw gradient ellipse
+
 	angleMode(DEGREES);
-	background(0);
-	translate(200, 200);
+	//translate(200, 200);
 	//noFill();
 
 	//rotate(frameCount * 1)
 
-	//ellipseSize = frameCount * 5;
-
 	let centerRing = 100
 
+	ellipse(0,0,100);
+
 	fill(centerRing,centerRing,centerRing,200);
-	strokeWeight(1);
+	strokeWeight(5);
 	stroke(centerRing);
 	ellipse(0,0,15/2+sizee, 15/2+sizee); //draw center ellipse
 
-	for(i=0;i<12;i++){ //draw flower of life
-		rotate(30);
-		stroke(50,50,20*i)
-		fill(50,50,20*i,satulation);
-		ellipse(0,-50,i/2+sizee, i/2+sizee);
-	}
+	// for(i=0;i<12;i++){ //draw flower of life
+	// 	rotate(30);
+	// 	stroke(50,50,20*i)
+	// 	fill(50,50,20*i,satulation);
+	// 	ellipse(0,-50,i/2+sizee, i/2+sizee);
+	// }
 
 	for(i=0;i<6;i++){ //draw seed of life
   		rotate(60);
-
-		
-		stroke(40*i,50,50);
-		fill(40*i,50,50,satulation);
+		//stroke(40*i,50,50);
+		//fill(40*i,50,50,satulation);
+		noFill();
   		ellipse(0,-25,i/2+sizee, i/2+sizee);
-		
 	}
 
 	sizee+=adder;
@@ -77,13 +69,10 @@ function draw_one_frame() {
 	}
 }
 
-// function polygon(x, y, radius, npoints) { //function for drawing polygons
-// 	let angle = TWO_PI / npoints;
-// 	beginShape();
-// 	for (let a = 0; a < TWO_PI; a += angle) {
-// 	  let sx = x + cos(a) * radius;
-// 	  let sy = y + sin(a) * radius;
-// 	  vertex(sx, sy);
-// 	}
-// 	endShape(CLOSE);   //polygon(0, 0, 70, 7); codee for drawing
-//   }
+function radialGradient(sX,sY,sR,eX,eY,eR,colorS,colorE){ //gradient codes
+	let gradient = drawingContext.createRadialGradient(sX,sY,sR,eX,eY,eR);
+	gradient.addColorStop(0, colorS);
+	gradient.addColorStop(1, colorE);
+	
+	drawingContext.fillStyle = gradient; 
+}
