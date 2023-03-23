0c05f8852b2ec417c42423315172621f64c226db
Kazumi <satokazumi69@gmail.com>
2023-03-23 16:27:26 +1300

    Update draw_one_frame.js

--- draw_one_frame.js
+++ draw_one_frame.js
@@ -1,12 +1,3 @@
-// var x=300;
-// var y=300;
-// var a=100;
-// var b=100;
-
-// var polySize=10
-// var deg24 = 24
-// var deg60 = 60
-
 var sizee; 
 var sizee2;
 var adder; 
@@ -15,54 +6,41 @@ var adder2;
 var seedSize = 50 //determines size of each seed ellipse 
 var gradSize = 150 //determines size of gradient ellipse
 
-sizee = 25; //determines size of the seed of life
-sizee2 = 125;
+sizee = 15; //determines size of the seed of life
+sizee2 = 115;
 adder = 4; //determines speed of ellipse size change
 adder2 = 4;
 
 function draw_one_frame() {
 	colorMode(HSB,360,100,100,1);
 	angleMode(DEGREES);
-	background(270,85,5);
+	background(280,100,20);
 	translate(width/2,height/2-50); //center of everything
 
 	scale(1.7);
 
-	push();
-	radialGradient(
-		0, 0, 50,
-		0, 0, 100,
-		color(50,100,100,100), //startcolor
-		color(50,20,100,0) //endcolor
-	  );
-	noStroke();
-	ellipse(0,0,250,250); //draw gradient ellipse
-	pop();
 
 	let centerRing = 100
 	
-	noFill();
-	strokeWeight(3);
-	stroke(0);
-	//ellipse(0,0,100);
-
-	//fill(centerRing,centerRing,centerRing,200);
 	ellipse(0,0,1/2+sizee, 1/2+sizee); //draw center ellipse
 
 	push();
   	for(ii=0;ii<6;ii++){ //draw outer seed of life
   		rotate(60);
+		stroke(360);
 		noFill();
 		push();
 		strokeradialGradient(
 			0, gradSize/2, 50,
-       		0, gradSize/2, 100,
+			0, gradSize/2, 100,
 			0, gradSize/2, 190,
-			color(270,85,5,0),
-			color(0,0,100,100),
-        	color(270,85,5,0) 
-	  	);
-		strokeWeight(30);
+			color(280,100,20,0),
+			color(50,40,100,100),
+			color(280,100,20,0)
+			);
+		noFill();
+		
+		strokeWeight(3);
 		ellipse(0,gradSize/2,ii/2+sizee2,ii/2+sizee2);
 		pop();
   	}
@@ -79,25 +57,45 @@ function draw_one_frame() {
 	pop();
 
 	push();
-	for(i=0;i<6;i++){ //draw inner seed of life
-  		rotate(60);
-		//stroke(40*i,50,50);
-		//fill(40*i,50,50,satulation);
-		stroke(0);
-		noFill();
-  		ellipse(0,seedSize/2,i/2+sizee, i/2+sizee);
-	}
-
-	sizee+=adder;
-
-	if (sizee >= seedSize) {
-  		adder*=-1;
-	} 
-
-	if (sizee <= seedSize-50) {
-  		adder*=-1;
-	}
+	radialGradient(
+		0, 0, 50,
+		0, 0, 100,
+		color(50,100,100,100), //startcolor
+		color(50,20,100,0) //endcolor
+	  );
+	noStroke();
+	ellipse(0,0,250,250); //draw gradient ellipse
 	pop();
+
+	push();
+	// for(i=0;i<6;i++){ //draw inner seed of life
+  	// 	rotate(60);
+	// 	push();
+	// 	strokeradialGradient(
+	// 	  0, seedSize/2, 50,
+	// 	  0, seedSize/2, 100,
+	// 	  0, seedSize/2, 190,
+	// 	  color(0,0,100,100),
+	// 	  color(270,85,5,0),
+	// 	  color(0,0,100,100) //endcolor
+	// 	);
+	// 	noFill();
+		  
+	// 	strokeWeight(3);
+	// 	ellipse(0,seedSize/2,i/2+sizee, i/2+sizee);
+	// 	pop();
+	// }
+
+	// sizee+=adder;
+
+	// if (sizee >= seedSize) {
+  	// 	adder*=-1;
+	// } 
+
+	// if (sizee <= seedSize-50) {
+  	// 	adder*=-1;
+	// }
+	// pop();
 
 	
 }
